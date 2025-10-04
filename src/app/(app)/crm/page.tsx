7f4d76a14da3369cf8387747da1584cce2e883b6
Rjkurti <info.rjkurti@gmail.com>
2025-10-04 05:05:37 +0000

    i click crm i show 404

---
+++ src/app/(app)/crm/page.tsx
@@ -0,0 +1,17 @@
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
+
+export default function CrmPage() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>CRM</CardTitle>
+        <CardDescription>
+          Manage your customer relationships and interactions.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>CRM functionality will be implemented here.</p>
+      </CardContent>
+    </Card>
+  );
+}
