a8d55dc03fe188f533f023d9b4d07953f9e4ab88
Rjkurti <info.rjkurti@gmail.com>
2025-10-05 08:52:24 +0000

    add my user in user table and give full permission to edit update and de

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 import type { Metadata } from 'next';
 import { Nav } from '@/components/nav';
@@ -83,3 +84,5 @@ export default function AppLayout({
         </div>
   );
 }
+
+    
